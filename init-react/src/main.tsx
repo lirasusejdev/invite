102a51efdafb84c0d5f37c9f437d3d6392195592
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-05-06 17:09:28 -0300

    add test

--- init-react/src/main.tsx
+++ init-react/src/main.tsx
@@ -7,4 +7,12 @@ ReactDOM.render(
     <App/>
   </React.StrictMode>
   document.getElementById('root')
+  
+  const person = {
+    name = 'Juan'
+    age = 30
+    weight = 88.6
+    isAdmin = true
+  }
+  console.log(person)
 )
