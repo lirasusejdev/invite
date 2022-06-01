4b521bc69a7c8532155ba75fd7f8e5ae176b17a2
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-06-01 14:37:53 -0300

    add json and App

--- init-react/src/App.tsx
+++ init-react/src/App.tsx
@@ -1,4 +1,36 @@
-function App() {
-  return <h1>Invite</h1>
+import React { Component } from 'react'
+import {ShepherdTour, ShepherdTourContext} from 'react-shepherd'
+import newSteps from './steps'
+
+const tourOptions = {
+  defaultStepOptions: {
+    cancelIcon: {
+      enabled: true
+    }
+  },
+  useModalOverlay: true
+};
+
+function Button() {
+  const tour = useContext(ShepherdTourContext);
+
+  return (
+    <button className="button dark" onClick={tour.start}>
+      Start Tour
+    </button>
+  );
 }
-export default App
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ShepherdTour steps={newSteps} tourOptions={tourOptions}>
+          <Button />
+        </ShepherdTour>
+      </div>
+    );
+}
+ 
+
+
